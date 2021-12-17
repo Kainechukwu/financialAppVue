21194d88012ba862f36d96feca5420f8d214d71c
kaine1 <kaine@onboardbase.com>
2021-12-17 16:07:17 +0100

    login in progress

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,5 +3,10 @@ export default [
 		path: "/signup",
 		name: "Signup",
 		component: () => import("@/views/signup/Signup.vue")
+	},
+	{
+		path: "/login",
+		name: "Login",
+		component: () => import("@/views/login/Login.vue")
 	}
 ]
