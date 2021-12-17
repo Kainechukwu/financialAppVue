e6d17154291fc304930493faecce0c987d678b00
kaine1 <kaine@onboardbase.com>
2021-12-17 12:02:11 +0100

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home bg-gray-200">
+  <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
