649f8206a124efcc8b8d3aeb748a9cc321f2d4b4
kaine1 <kaine@onboardbase.com>
2022-01-27 14:43:36 +0100

    get started page done

--- src/views/main/PendingTasks.vue
+++ src/views/main/PendingTasks.vue
@@ -15,28 +15,32 @@
 							<CompleteProfileDetailsSvg />
 							<p class="ml-4 my-auto inter fs-14 fw-400">Complete profile details</p>
 						</div>
-						<CheckedSvg />
+						<CheckedSvg v-if="isProfileUpdated" />
+						<GreenCheckedSvg v-else />
 					</div>
 					<div @click="goToProfileSettings" class="cursor-pointer flex px-6 py-4 justify-between">
 						<div class="flex">
 							<VerifyContactSvg />
 							<p class="ml-4 my-auto inter fs-14 fw-400">Verify Phone Number</p>
 						</div>
-						<CheckedSvg />
+						<CheckedSvg v-if="isPhoneNumberVerified" />
+						<GreenCheckedSvg v-else />
 					</div>
 					<div @click="goToCompliance" class="cursor-pointer flex px-6 py-4 justify-between">
 						<div class="flex">
 							<KycSvg />
 							<p class="ml-4 my-auto inter fs-14 fw-400">KYC: Upload ID, Take selfie</p>
 						</div>
-						<CheckedSvg />
+						<CheckedSvg v-if="isKycDone" />
+						<GreenCheckedSvg v-else />
 					</div>
 					<div @click="goToPinSettings" class="cursor-pointer flex px-6 py-4 justify-between">
 						<div class="flex">
 							<SetPinSvg />
 							<p class="ml-4 my-auto inter fs-14 fw-400">Set authenticaton PIN</p>
 						</div>
-						<CheckedSvg />
+						<CheckedSvg v-if="hasPin" />
+						<GreenCheckedSvg v-else />
 					</div>
 				</div>
 			</div>
@@ -50,18 +54,27 @@ import VerifyContactSvg from "@/components/svg/VerifyContactSvg.vue";
 import SetPinSvg from "@/components/svg/SetPinSvg.vue";
 import CheckedSvg from "@/components/svg/CheckedSvg.vue";
 import KycSvg from "@/components/svg/KycSvg.vue";
+import GreenCheckedSvg from "@/components/svg/GreenCheckedSvg.vue";
 import { useRouter } from "vue-router";
+import { useStore } from "vuex";
 
 export default {
 	name: "Pending Tasks",
 	components: {
 		CompleteProfileDetailsSvg,
+		GreenCheckedSvg,
 		CheckedSvg,
 		KycSvg,
 		SetPinSvg,
 		VerifyContactSvg,
 	},
 	setup() {
+		const store = useStore();
+		const hasPin = store.getters["authToken/hasPin"];
+		const isProfileUpdated = store.getters["authToken/isProfileUpdated"];
+		const isPhoneNumberVerified = store.getters["authToken/isPhoneNumberVerified"];
+		const isKycDone = store.getters["authToken/isKycDone"];
+
 		const router = useRouter();
 		const goToProfileSettings = () => {
 			router.push("/settings/profile");
@@ -76,6 +89,10 @@ export default {
 			goToProfileSettings,
 			goToPinSettings,
 			goToCompliance,
+			hasPin,
+			isProfileUpdated,
+			isPhoneNumberVerified,
+			isKycDone,
 		};
 	},
 };
