ec0f1c3865d496413b865102e4e0541a41155d0d
kaine1 <kaine@onboardbase.com>
2022-02-01 09:06:35 +0100

    getting transaction log narration

--- src/views/main/earn/AccountFundingListItem.vue
+++ src/views/main/earn/AccountFundingListItem.vue
@@ -3,7 +3,7 @@
 		<AccountFundingListItemSvg />
 		<div class="flex flex-col w-full pl-6 pr-4">
 			<div class="flex justify-between">
-				<h1 class="inter fw-600 fs-14 thTypeListColor">Account Funding</h1>
+				<h1 class="inter fw-600 fs-14 thTypeListColor">{{ transaction.narration }}</h1>
 				<span class="black-text inter fw-700 fs-12">{{ curr }} {{ transaction.currency }}</span>
 			</div>
 			<div class="flex justify-between">
