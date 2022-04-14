67074febdedfd460a919b79cf65ce5498060b399
kaine1 <kaine@onboardbase.com>
2022-04-14 12:18:16 +0100

    product cards updated

--- src/views/LandingPage/ProductCards.vue
+++ src/views/LandingPage/ProductCards.vue
@@ -3,20 +3,22 @@
 		style="background: rgba(255, 255, 255, 0.5); border-radius: 20px"
 		class="productCard col-span-1 z-10 px-6 py-8"
 	>
-		<div class="flex flex-col">
+		<div class="flex flex-col h-full relative">
 			<div class="my-2">
 				<slot></slot>
 			</div>
 
 			<span class="mt-2 brFirmaBold fw-700 fs-18 mb-3" style="color: #1e2235">{{ header }} </span>
-			<span style="line-height: 21px" class="brFirmaReg fw-400 fs-14">{{ description }}</span>
+			<span style="line-height: 21px" class="mb-6 brFirmaReg fw-400 fs-14">{{ description }}</span>
 
-			<div class="mt-6 flex items-center">
-				<span
-					class="brFirmaSB fw-600 fs-14"
+			<div class="flex items-center mt-auto">
+				<div
+					style="height: 40px"
 					v-if="header === 'Multi-Currency Account' || header === 'USDC Backed Debit Cards'"
-					>Coming soon</span
+					class="flex items-center"
 				>
+					<span class="brFirmaSB fw-600 fs-14">Coming soon</span>
+				</div>
 				<button
 					v-else
 					class="brFirmaReg fw-400 fs-14"
